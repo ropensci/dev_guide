40f2746cf4c87c6e0c3df91d22fd2ef424d1539b
Maëlle Salmon <maelle@itsalocke.com>
2018-06-12 07:00:00 +0200

    new try à la tic

--- _deploy.sh
+++ _deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 set -e
-set -x
 
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
@@ -9,15 +8,9 @@ set -x
 git config --global user.email "maelle.salmon@yahoo.se"
 git config --global user.name "Maëlle Salmon"
 
-DOCS_DIR=docs
-REMOTE_URL="https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git"
-
-mkdir -p ${DOCS_DIR}
-rm -rf ${DOCS_DIR}/.git
-touch ${DOCS_DIR}/.nojekyll
-git init ${DOCS_DIR}
-git -C ${DOCS_DIR} checkout --orphan gh-pages
-git -C ${DOCS_DIR} add .
-git -C ${DOCS_DIR} commit --no-verify -m "Auto-build ($TRAVIS_COMMIT_MESSAGE)" || true
-git -C ${DOCS_DIR} remote add origin -m "gh-pages" ${REMOTE_URL}
-git -C ${DOCS_DIR} push --force -u origin gh-pages
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+cd book-output
+cp -r ../_book/* ./
+git add --all *
+git commit -m"Update the book" || true
+git push -q origin gh-pages
