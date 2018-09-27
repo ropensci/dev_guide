54047b287d1025da54be1c13feb05bdec18f0a44
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-09-27 20:47:36 +0200

    only keep necessary stuff and add deploy from dev

--- tic.R
+++ tic.R
@@ -1,27 +1,34 @@
-if (ci()$get_branch() == "master"){
-  get_stage("before_install") %>%
-    add_code_step(update.packages(ask = FALSE))
+get_stage("before_install") %>%
+  add_code_step(update.packages(ask = FALSE))
 
-  get_stage("install") %>%
-    add_code_step(remotes::install_deps(dependencies = TRUE))
+get_stage("install") %>%
+  add_code_step(remotes::install_deps(dependencies = TRUE))
 
-  get_stage("deploy") %>%
-    add_code_step(
-      bookdown::render_book('index.Rmd', 'bookdown::gitbook'),
-      prepare_call = remotes::install_github("rstudio/bookdown")
-    )
+get_stage("deploy") %>%
+  add_code_step(
+    bookdown::render_book('index.Rmd', 'bookdown::gitbook'),
+    prepare_call = remotes::install_github("rstudio/bookdown")
+  )
 
-  if (Sys.getenv("id_rsa") != "") {
-    # pkgdown documentation can be built optionally. Other example criteria:
-    # - `inherits(ci(), "TravisCI")`: Only for Travis CI
-    # - `ci()$is_tag()`: Only for tags, not for branches
-    # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
-    # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
-    get_stage("before_deploy") %>%
-      add_step(step_setup_ssh())
+if (Sys.getenv("id_rsa") != "") {
+  # pkgdown documentation can be built optionally. Other example criteria:
+  # - `inherits(ci(), "TravisCI")`: Only for Travis CI
+  # - `ci()$is_tag()`: Only for tags, not for branches
+  # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
+  # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
+  get_stage("before_deploy") %>%
+    add_step(step_setup_ssh())
 
+  if (ci()$get_branch() == "master" || ci()$is_tag()) {
     get_stage("deploy") %>%
       add_step(step_push_deploy(path = "_book", branch = "gh-pages"))
+  }
+
+  ## if the branch is dev the lesson gets deployed to the
+  ## development branch, and will be rendered by netlify
+  if (ci()$get_branch() == "dev") {
+    get_stage("deploy") %>%
+      add_step(step_push_deploy(path = "_book", branch = "dev-site"))
   }
 
 }
