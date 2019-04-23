0e697bd3a75d362bd4e45b91e1d0bbb0d344fc82
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-04-23 10:50:32 +0200

    fix tic?

--- tic.R
+++ tic.R
@@ -16,7 +16,8 @@ get_stage("deploy") %>%
   add_code_step(
     file.copy(from = "pdfbook/ropensci-dev-guide.pdf",
               to = "_book/ropensci-dev-guide.pdf")
-
+  ) %>%
+  add_code_step(
     purrr::walk(
       list.files("images", full.names = TRUE),
       file.copy,
