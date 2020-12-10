e55277d8ef1e7aa625e13e6c476b5c1973ba185b
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-12-10 16:58:20 +0100

    find external contributors

--- inst/thanks.R
+++ inst/thanks.R
@@ -30,3 +30,18 @@ changelog_thanks <- readLines("appendix.Rmd") %>%
   unique()
 
 changelog_thanks[!changelog_thanks %in% index_thanks]
+
+thanks2020 <- readLines("appendix.Rmd")[1:83] %>%
+  blogdown:::split_yaml_body() %>%
+  .$body %>%
+  commonmark::markdown_xml(extensions = TRUE) %>%
+  xml2::read_xml() %>%
+  xml2::xml_find_all(xpath = './/d1:code', xml2::xml_ns(.)) %>%
+  xml2::xml_text() %>%
+  purrr::keep(function(x) grepl("^@", x)) %>%
+  stringr::str_remove_all("^@") %>%
+  tolower() %>%
+  unique()
+
+thanks2020[!thanks2020 %in% c("includermd", "example", "sckott", "karthik", "annakrystalli",
+                              "noamross", "stefaniebutland", "jeroen", "mpadge")]
