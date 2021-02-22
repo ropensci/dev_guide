82be00a61d1dbd3e5b8449ecfcf0e02011371f8d
Maëlle Salmon <maelle.salmon@yahoo.se>
2021-02-22 08:42:12 +0100

    Dev Guide 0.6.0 (#298)

---
+++ inst/thanks.R
@@ -0,0 +1,47 @@
+library("magrittr")
+index_thanks <- readLines("index.Rmd") %>%
+  blogdown:::split_yaml_body() %>%
+  .$body %>%
+  commonmark::markdown_xml(extensions = TRUE) %>%
+  xml2::read_xml() %>%
+  xml2::xml_find_all(xpath = './/d1:link', xml2::xml_ns(.)) %>%
+  xml2::xml_attr("destination") %>%
+  urltools::url_parse() %>%
+  dplyr::filter(
+    domain == "github.com"
+    ) %>%
+  dplyr::mutate(
+    path = gsub("\\/$", "", path)
+  ) %>%
+  dplyr::filter(
+    !grepl("\\/", path)
+  ) %>%
+  dplyr::pull(path)
+
+changelog_thanks <- readLines("appendix.Rmd") %>%
+  blogdown:::split_yaml_body() %>%
+  .$body %>%
+  commonmark::markdown_xml(extensions = TRUE) %>%
+  xml2::read_xml() %>%
+  xml2::xml_find_all(xpath = './/d1:code', xml2::xml_ns(.)) %>%
+  xml2::xml_text() %>%
+  purrr::keep(function(x) grepl("^@", x)) %>%
+  stringr::str_remove_all("^@") %>%
+  unique()
+
+changelog_thanks[!changelog_thanks %in% index_thanks]
+
+thanks2020 <- readLines("appendix.Rmd")[1:83] %>%
+  blogdown:::split_yaml_body() %>%
+  .$body %>%
+  commonmark::markdown_xml(extensions = TRUE) %>%
+  xml2::read_xml() %>%
+  xml2::xml_find_all(xpath = './/d1:code', xml2::xml_ns(.)) %>%
+  xml2::xml_text() %>%
+  purrr::keep(function(x) grepl("^@", x)) %>%
+  stringr::str_remove_all("^@") %>%
+  tolower() %>%
+  unique()
+
+thanks2020[!thanks2020 %in% c("includermd", "example", "sckott", "karthik", "annakrystalli",
+                              "noamross", "stefaniebutland", "jeroen", "mpadge")]
