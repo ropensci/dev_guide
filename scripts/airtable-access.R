96cb39710940c1c6bbbec0813dbcdd3a66780ef4
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-02-23 10:58:30 +0100

    use edit

--- scripts/airtable-access.R
+++ scripts/airtable-access.R
@@ -1,10 +1,10 @@
-has_airtable_access <- function() {
+has_airtable_access <- function(env = .GlobalEnv) {
   has_airtable_access <- FALSE
   if (Sys.getenv("AIRTABLE_API_KEY") != ""){
     has_airtable_access <- TRUE
   } else {
     # if key is in params then set as envvar:
-    if ("params" %in% ls()) {
+    if ("params" %in% ls(envir = env)) {
       if ("AIRTABLE_API_KEY" %in% names(params)){
         Sys.setenv(AIRTABLE_API_KEY = params$AIRTABLE_API_KEY)
         has_airtable_access <- TRUE
