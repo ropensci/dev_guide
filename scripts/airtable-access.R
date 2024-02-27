feb86c6cc240567a130e54b7b88f77c56b3e12dc
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-02-27 12:35:46 +0100

    simplify code

--- scripts/airtable-access.R
+++ scripts/airtable-access.R
@@ -1,15 +1,15 @@
 has_airtable_access <- function(env = .GlobalEnv) {
-  has_airtable_access <- FALSE
+
   if (Sys.getenv("AIRTABLE_API_KEY") != ""){
-    has_airtable_access <- TRUE
-  } else {
-    # if key is in params then set as envvar:
-    if ("params" %in% ls(envir = env)) {
-      if ("AIRTABLE_API_KEY" %in% names(params)){
-        Sys.setenv(AIRTABLE_API_KEY = params$AIRTABLE_API_KEY)
-        has_airtable_access <- TRUE
-      }
+    return(TRUE)
+  }
+
+  if ("params" %in% ls(envir = env)) {
+    if ("AIRTABLE_API_KEY" %in% names(params)){
+      Sys.setenv(AIRTABLE_API_KEY = params$AIRTABLE_API_KEY)
+      return(TRUE)
     }
   }
-  has_airtable_access
+
+  FALSE
 }
