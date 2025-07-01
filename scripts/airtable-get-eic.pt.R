3d29603f8e0236b6602bd32f6fb57185d92c5fdc
Maëlle Salmon <maelle.salmon@yahoo.se>
2025-07-01 16:25:03 +0200

    quick fix?

---
+++ scripts/airtable-get-eic.pt.R
@@ -0,0 +1,9 @@
+out <- paste0(
+  "Nós alternamos nosso editor-chefe, geralmente a cada três meses. ",
+  "Nosso editor-chefe atual é[",
+  eic_name,
+  "](https://github.com/",
+  eic_github,
+  ").\n"
+)
+cat(out, sep = "")
